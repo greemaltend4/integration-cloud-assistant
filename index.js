b86c69108397ef98ec139f88ec045cf23d2125f9
greemaltend4 <dengestabiy@hotmail.com>
2025-10-01 15:24:28 +0000

    Add a greeting message before starting the integration assistant.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ console.log('Welcome to the Cloud Assistant Integration!');
 // Entry points for integration deployment
 
 function startAssistant() {
+    console.log('Hello! Preparing to initiate the integration assistant...');
     console.log('Starting the integration assistant...');
     // Integration logic here
 }
