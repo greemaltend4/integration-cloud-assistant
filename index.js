cf6493f4d040b768d44bcfe8ae2dd91e2eb29bf7
greemaltend4 <dengestabiy@hotmail.com>
2025-10-01 15:22:29 +0000

    Fix typo in log messages.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-console.log('Bienvenue dans lIntegration Cloud Assistant!');
+console.log('Bienvenue dans l'intégration Cloud Assistant!');
 
 // Points d'entrée pour le déploiement des intégrations
 
 function startAssistant() {
-    console.log('Démarrage de lassistant dintégration...');
+    console.log('Démarrage de l'assistant d'intégration...');
     // Logique d'intégration ici
 }
 
