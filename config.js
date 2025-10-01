816318b20aa06f2da42b1b627b761f01e7b20225
greemaltend4 <dengestabiy@hotmail.com>
2025-10-01 15:22:02 +0000

    feat: initial commit for config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+// Configuration pour l'intégration des services cloud
+
+const config = {
+    aws: {
+        accessKeyId: '',
+        secretAccessKey: '',
+        region: ''
+    },
+    azure: {
+        clientId: '',
+        clientSecret: '',
+        tenantId: ''
+    },
+    googleCloud: {
+        projectId: '',
+        keyFilename: ''
+    }
+};
+
+module.exports = config;
